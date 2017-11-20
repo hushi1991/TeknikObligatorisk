584169f13437817348330f2afa34c3acb02f55b3
hushi1991 <davb@itu.dk>
2017-11-20 17:38:56 +0100

    Test upload

---
+++ TeknikObligatorisk/src/main/java/com/example/demo/Model/Entities/Test.java
@@ -0,0 +1,17 @@
+package com.example.demo.Model.Entities;
+
+/**
+ * Created by David on 20-11-2017.
+ */
+public class Test {
+
+    private String tekst;
+
+
+    public Test(String tekst) {
+        this.tekst = "Hej dette er en test";
+    }
+
+
+}
+
